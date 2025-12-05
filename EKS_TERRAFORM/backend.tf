26760281d9e612b07648052c1e429621644cc434
dipali57 <dipalicg2020@gmail.com>
2025-12-05 15:29:50 +0530

    First commit

---
+++ EKS_TERRAFORM/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "devsecops-2048-reyaz-2" # Replace with your actual S3 bucket name
+    key    = "EKS/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
